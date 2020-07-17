5356ab473870d26e282f8990d2f6d3e1fda0f14e
WebKawsar <web.kawsarahmed@gmail.com>
2020-07-17 16:28:07 +0600

    4ta solve korlam

--- assignment.js
+++ assignment.js
@@ -10,9 +10,9 @@
 ******************************************************************/
 //feetToMile
 
-function feetToMile(feet){
-    getAbsoluteFeet = Math.abs(feet);
-    mile = getAbsoluteFeet / 5280; 
+function feetToMile(feet) {
+    getAbsoluteFeet = Math.abs(feet); // Get Absolute Value of Feet
+    mile = getAbsoluteFeet / 5280;
     return mile;
 }
 
@@ -26,18 +26,18 @@ var resultMile = feetToMile(feet);
 ******************************************************************/
 //tinyFriend
 
-function tinyFriend(friendsName){
+function tinyFriend(friendsName) {
 
     var nameWordCount = friendsName[0].length;
     var smallFriendName = [];
-    for(var i=0; i<friendsName.length; i++){
+    for (var i = 0; i < friendsName.length; i++) {
 
         element = friendsName[i].length;
-        if(element < nameWordCount){
+        if (element < nameWordCount) {
             nameWordCount = element;
             smallFriendName = friendsName[i];
         }
-    
+
     }
     return smallFriendName;
 
@@ -55,25 +55,25 @@ var getSmallName = tinyFriend(name);
 ******************************************************************/
 //brickCalculator
 
-function brickCalculator(floorNo){
+function brickCalculator(floorNo) {
 
-    var heightOneFeet = 1000;
-    var fiftinFeetHeight = heightOneFeet * 15;
-    var twelveFeetHeight = heightOneFeet * 12;
-    var tenFeetHeight = heightOneFeet * 10;
+    const heightOneFeet = 1000;
+    const fiftinFeetHeight = heightOneFeet * 15;
+    const twelveFeetHeight = heightOneFeet * 12;
+    const tenFeetHeight = heightOneFeet * 10;
 
-    if(floorNo <= 10){
+    if (floorNo <= 10) {
 
         bricksAreNeed = fiftinFeetHeight * floorNo;
-    } 
-    else if(10 < floorNo && floorNo <= 20){
+    }
+    else if (10 < floorNo && floorNo <= 20) {
 
         bricksNeedTenFloor = (fiftinFeetHeight * 10);
         bricksNeedAdditional = (floorNo - 10) * twelveFeetHeight;
         bricksAreNeed = bricksNeedTenFloor + bricksNeedAdditional;
 
-    } 
-    else if(floorNo > 20){
+    }
+    else if (floorNo > 20) {
 
         bricksNeedTenFloor = (fiftinFeetHeight * 10);
         bricksNeedTwentyFloor = (twelveFeetHeight * 10);
@@ -89,9 +89,39 @@ function brickCalculator(floorNo){
 var floor = 32;
 
 getResult = brickCalculator(floor);
+//console.log(getResult);
+
+
+
+/**************************************************************** 
+                        No. of Assignment-2
+******************************************************************/
+//woodCalculator
+
+function woodCalculator(chairQty, tableQty, bedQty) {
+
+    getAbsoluteChair = Math.abs(chairQty);  // Get Absolute Value of Chair Quantity
+    getAbsoluteTable = Math.abs(tableQty);  // Get Absolute Value of Table Quantity
+    getAbsoluteBed   = Math.abs(bedQty);      // Get Absolute Value of Bed Quantity
+
+    const oneChair = 1;
+    const oneTable = 3;
+    const oneBed   = 5;
+
+    woodNeedForChair = oneChair * getAbsoluteChair;
+    woodNeedForTable = oneTable * getAbsoluteTable;
+    woodNeedForBed   = oneBed * getAbsoluteBed;
+
+    totalWoodNeed = woodNeedForChair + woodNeedForTable + woodNeedForBed;
+    return totalWoodNeed;
+}
+
+var chair = 5;
+var table = 5;
+var bed = 5;
+
+var getResult = woodCalculator(chair, table, bed);
 console.log(getResult);
-
-
 
 
 
